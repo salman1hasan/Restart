93058f7f11e178d868df59e5d5631df3dcfbccdf
Salman Hasan <s.hasan@unguarded.com>
2023-09-27 14:28:44 -0400

    Update

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -32,70 +32,64 @@ struct OnBoardingView: View {
                         .padding(.horizontal,10)
                 }
                 ZStack{
+                    CircleGroupView(ShapeColor:.white, ShapeOpacity: 0.2)
+                        Image("character-1")
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    
+                    Spacer()
+                    
                     ZStack{
-                        Circle()
-                            .stroke(.white.opacity(0.2),lineWidth:40)
-                            .frame(width:260, height:260, alignment: .center)
-                        Circle()
-                            .stroke(.white.opacity(0.2), lineWidth: 80)
-                            .frame(width:260, height: 260, alignment:.center)
-                    }
-                    Image("character-1")
-                        .resizable()
-                        .scaledToFit()
-                }
-                
-                Spacer()
-                
-                ZStack{
-                    Capsule()
-                        .fill(Color.white.opacity(0.2))
-                    
-                    Capsule()
-                        .fill(Color.white.opacity(0.2))
-                        .padding(8)
-                    
-                    Text("Get Started")
-                        .font(.system(.title3,design: .rounded))
-                        .fontWeight(.bold)
-                        .foregroundColor(.white)
-                        .offset(x:20)
-                    
-                    HStack{
                         Capsule()
-                            .fill(Color("ColorRed"))
-                            .frame(width:80)
-                        Spacer()
+                            .fill(Color.white.opacity(0.2))
+                        
+                        Capsule()
+                            .fill(Color.white.opacity(0.2))
+                            .padding(8)
+                        
+                        Text("Get Started")
+                            .font(.system(.title3,design: .rounded))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .offset(x:20)
+                        
+                        HStack{
+                            Capsule()
+                                .fill(Color("ColorRed"))
+                                .frame(width:80)
+                            Spacer()
+                        }
+                        
+                        
+                        HStack{
+                            ZStack{
+                                Circle()
+                                    .fill(Color("ColorRed"))
+                                Circle()
+                                    .fill(.black.opacity(0.15))
+                                    .padding(8)
+                                Image(systemName:"chevron.right.2")
+                                    .font(.system(size:24, weight:.bold))
+                            }
+                            .foregroundColor(.white)
+                            .frame(width:80, height:80, alignment: .center)
+                            .onTapGesture{
+                                isOnboardingViewActive = false
+                            }
+                            Spacer()
+                        }
                     }
-                    
-                    
-            HStack{
-              ZStack{
-                 Circle()
-                    .fill(Color("ColorRed"))
-                Circle()
-                    .fill(.black.opacity(0.15))
-                    .padding(8)
-            Image(systemName:"chevron.right.2")
-                .font(.system(size:24, weight:.bold))
-            }
-            .foregroundColor(.white)
-            .frame(width:80, height:80, alignment: .center)
-            .onTapGesture{
-                isOnboardingViewActive = false
-            }
-            Spacer()
-            }
-          }
-            .frame(height:80, alignment:.center)
-            .padding()
+                    .frame(height:80, alignment:.center)
+                    .padding()
+                }
             }
         }
     }
-}
-
-struct OnBoardingView_Previews: PreviewProvider {
-    static var previews: some View {
-        OnBoardingView()
+    
+    struct OnBoardingView_Previews: PreviewProvider {
+        static var previews: some View {
+            OnBoardingView()
+        }
     }
-}
+
